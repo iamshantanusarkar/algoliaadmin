8fc01bda476f7c0b5b6617b47970ad43eef897fb
Shantanu Sarkar <iamshantanusarkar@gmail.com>
2019-08-01 22:12:55 +0530

    update

--- frontend/src/app/pages/pages.component.ts
+++ frontend/src/app/pages/pages.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-root',
   template: `<layout-grid-with-sidebar><router-outlet></router-outlet></layout-grid-with-sidebar>`,
 })
 export class PagesComponent {}
