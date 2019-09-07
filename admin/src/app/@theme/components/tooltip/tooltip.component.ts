a53c8975d90818ac2d9500dbfb28ba5419bd02ff
Shantanu Sarkar <iamshantanusarkar@gmail.com>
2019-09-07 11:51:47 +0530

    update

---
+++ admin/src/app/@theme/components/tooltip/tooltip.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
+
+import { TooltipConfig } from './tooltip.config';
+
+@Component({
+  selector: '[data-tooltip]',
+  template: ``,
+  host: {
+  },
+  styleUrls: ['']
+})
+
+export class TooltiComponent implements OnInit, OnChanges {
+
+  constructor(
+    config: TooltipConfig,
+    // private _renderer: Renderer2,
+    // private _element: ElementRef
+  ) {}
+
+  //closeHandler() { this.close.emit(null); }
+
+  ngOnChanges() {
+    // const typeChange = changes['type'];
+    // if (typeChange && !typeChange.firstChange) {
+    //   this._renderer.removeClass(this._element.nativeElement, `alert-${typeChange.previousValue}`);
+    //   this._renderer.addClass(this._element.nativeElement, `alert-${typeChange.currentValue}`);
+    // }
+  }
+
+  ngOnInit() {
+    // this._renderer.addClass(this._element.nativeElement, `alert-${this.type}`);
+  }
+}
--- admin/src/app/@theme/components/tooltip/tooltip.module.ts
+++ admin/src/app/@theme/components/tooltip/tooltip.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TooltipConfig } from './tooltip.config';
 
 @NgModule({
-  declarations: [],
+  declarations: [ TooltipConfig ],
   imports: [
     CommonModule
   ]
