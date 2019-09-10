ef834d936ab6428312060f176895171fc1d30645
Shantanu Sarkar <iamshantanusarkar@gmail.com>
2019-09-10 20:58:36 +0530

    update

--- admin/src/app/pages/pages-routing.module.ts
+++ admin/src/app/pages/pages-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserManagementRoutingModule } from './user-management/user-management-routing.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -9,9 +10,19 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'settings', loadChildren: () => import('./settings/settings.module').then(mod => mod.SettingsModule) },
-      { path: 'report', loadChildren: () => import('./reports/reports.module').then(mod => mod.ReportsModule) },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: 'settings',
+        loadChildren: () => import('./settings/settings.module').then(mod => mod.SettingsModule)
+      }, {
+        path: 'report',
+        loadChildren: () => import('./reports/reports.module').then(mod => mod.ReportsModule)
+      }, {
+        path: 'user-management',
+        loadChildren: () => import('./user-management/user-management.module').then(mod => mod.UserManagementModule)
+      },
     ]
   }
 ];
--- admin/src/app/pages/reports/reports-routing.module.ts
+++ admin/src/app/pages/reports/reports-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportsComponent } from './reports.component';
+import { CustomerReportComponent } from './customer/customer-report.component';
+import { OrderReportComponent } from './order/order-report.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ReportsComponent },
+  { path: 'customer', component: CustomerReportComponent },
+  { path: 'order', component: OrderReportComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
