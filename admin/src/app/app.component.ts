33837defd82f1bc6624a6fc809b6352144047c3b
Shantanu Sarkar <iamshantanusarkar@gmail.com>
2019-09-13 01:38:46 +0530

    update

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  NgZone,
+  Renderer,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
 
 @Component({
     selector: '#admin-app-root',
@@ -7,9 +23,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-    constructor(
-    ) {}
+  @ViewChild('spinnerElement', {static: false})
+  spinnerElement: ElementRef;
 
-    ngOnInit() {
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    private renderer: Renderer
+  ) {
+    router.events.subscribe((event: RouterEvent) => {
+      this._navigationInterceptor(event);
+    });
+  }
+
+  private _navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.ngZone.runOutsideAngular(() => {
+        this.renderer.setElementStyle(
+          this.spinnerElement.nativeElement,
+          'display',
+          'block'
+        );
+      });
     }
+    if (event instanceof NavigationEnd) {
+      this._hideSpinner();
+    }
+    if (event instanceof NavigationCancel) {
+      this._hideSpinner();
+    }
+    if (event instanceof NavigationError) {
+      this._hideSpinner();
+    }
+  }
+
+  private _hideSpinner(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.renderer.setElementStyle(
+        this.spinnerElement.nativeElement,
+        'display',
+        'none'
+      );
+    });
+  }
+
+  ngOnInit() {}
 }
